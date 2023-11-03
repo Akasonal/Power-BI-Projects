a71b9d539de0c49725f3b218dd0241db09189e36
Akhil Anand <34447835+Akasonal@users.noreply.github.com>
2023-11-03 16:40:41 +0530

    file_consolidator.py

--- file_consolidator.py
+++ file_consolidator.py
@@ -6,14 +6,6 @@ import base64
 # Define the app title and page layout
 st.title("Finance File Consolidator")
 
-# Function to create a download link for a binary file
-def get_binary_file_downloader_html(bin_file, file_label='File'):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    b64 = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{b64}" download="consolidated_data.xlsx">Download Excel File</a>'
-    return href
-
 # Upload multiple Excel files
 uploaded_files = st.file_uploader("Upload multiple Excel files (xlsx)", type=["xlsx"], accept_multiple_files=True)
 
@@ -39,17 +31,13 @@ if uploaded_files:
     st.header("Data Overview")
     st.write(consolidated_data)  # Display the data overview on the main page
 
-    # Create a progress bar for the data overview
-    progress_bar = st.progress(0)  # Initialize the progress bar
-
-    # Update the progress bar
-    for i in range(100):
-        progress_bar.progress((i + 1) / 100)
-    
     # Save consolidated data to an Excel file
     excel_file = BytesIO()
     with pd.ExcelWriter(excel_file, engine="openpyxl", mode="w") as writer:
         consolidated_data.to_excel(writer, index=False, sheet_name="Consolidated Data")
 
     # Create a download button for the consolidated Excel file
-    st.markdown(get_binary_file_downloader_html(excel_file, "Download Excel File"), unsafe_allow_html=True)
+    download_link = st.empty()
+    excel_file.seek(0)
+    b64 = base64.b64encode(excel_file.read()).decode()
+    download_link.markdown(f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="consolidated_data.xlsx">Download Excel File</a>', unsafe_allow_html=True)
